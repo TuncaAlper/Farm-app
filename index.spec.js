f2fe5c4ae0287218bfb372d4b3f7cc949d26b6b9
Bencilki <alpertunca@gmail.com>
2019-02-20 12:43:52 +0100

    created Farm with some crops and animals

---
+++ index.spec.js
@@ -0,0 +1,33 @@
+const {Farm} = require("./index")
+const {Wheat, Sugarcane} = require("./crops")
+
+//test create new farm
+test("A new Farm can be created", ()=> {
+  const farm = new Farm()
+  expect(farm).toBeTruthy()
+})
+//test farm class can have a name
+test('A Farm can have a name', () => {
+  const farm = new Farm('TEST_NAME')
+  expect(farm.name).toBe('TEST_NAME')
+})
+
+//testing Farm classs - adding new crops
+test('We can add a crop to our farm', () => {
+  const farm = new Farm('TEST_NAME')
+  farm.addCrop(new Wheat(100))
+})
+
+//testing total income from crops 
+//(first is it array or value and then the value is correct or not?
+test('The income of an empty farm is 0', () => {
+  const farm = new Farm('TEST_NAME')
+  expect(farm.calculateIncome()).toBe(0)
+})
+test('The income of a 100 Wheat + 100 Sugarcane farm is X', () => {
+  const farm = new Farm('TEST_NAME')
+  farm.addCrop(new Wheat(100))
+  farm.addCrop(new Sugarcane(100))
+  expect(farm.calculateIncome()).toBeCloseTo(1918.37, 1)
+})
+  
